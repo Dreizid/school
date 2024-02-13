4c116169710e743f79cb6823eca3d3ff61926db0
Reiii laptop <adolfojanandreim@gmail.com>
2024-02-13 19:29:29 +0800

    added the rest of the item types to Items.java

--- java/inter_prog/final_project/src/core/Items.java
+++ java/inter_prog/final_project/src/core/Items.java
@@ -1,12 +1,26 @@
 package core;
-import java.util.HashMap;
 
 public class Items {
-    // public static HashMap<String, MeatItem> meatList = new HashMap<>();
-    public static MeatItem[] meatList = {new MeatItem("Chicken", "Chickey", "null", "1 whole", 127.30),
+    // name: Name, description: Kahit wala muna lagay nyolang null, imagePath: format src\\gui\\static\\images\\(itemname).png, unit: kung ilang kg, price: price
+    public static MeatItem[] meatList = {
+                                        new MeatItem("Chicken", "Chickey", "null", "1 whole", 127.30),
                                         new MeatItem("Beef", "Beefy", "src\\gui\\static\\images\\beef.png", "1kg", 155.50),
                                         new MeatItem("Pork", "", "", "1kg", 230.5)
-};
+    };
+
+    public static FishItem[] fishList = {
+                                        new FishItem("name", "description", "imagepath", "unit", 0)
+    };
+
+    public static FruitItem[] fruitList = {
+                                        new FruitItem("name", "description", "imagepath", "unit", 0)
+    };
+
+    public static VegetableItem[] vegetableList = {
+                                        new VegetableItem("name", "description", "imagepath", "unit", 0)
+    };
+
+
     public Items() {
 
     }
