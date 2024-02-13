9f7df0ac6cd59a33103d91c37431ba28acb9cd4f
Reiii laptop <adolfojanandreim@gmail.com>
2024-02-13 19:32:26 +0800

    assigned members to populate items in Items.java

--- java/inter_prog/final_project/src/core/Items.java
+++ java/inter_prog/final_project/src/core/Items.java
@@ -2,20 +2,25 @@ package core;
 
 public class Items {
     // name: Name, description: Kahit wala muna lagay nyolang null, imagePath: format src\\gui\\static\\images\\(itemname).png, unit: kung ilang kg, price: price
+    
+    // Justine
     public static MeatItem[] meatList = {
                                         new MeatItem("Chicken", "Chickey", "null", "1 whole", 127.30),
                                         new MeatItem("Beef", "Beefy", "src\\gui\\static\\images\\beef.png", "1kg", 155.50),
                                         new MeatItem("Pork", "", "", "1kg", 230.5)
     };
 
+    // Ein
     public static FishItem[] fishList = {
                                         new FishItem("name", "description", "imagepath", "unit", 0)
     };
 
+    // Justine
     public static FruitItem[] fruitList = {
                                         new FruitItem("name", "description", "imagepath", "unit", 0)
     };
 
+    // Ein
     public static VegetableItem[] vegetableList = {
                                         new VegetableItem("name", "description", "imagepath", "unit", 0)
     };
